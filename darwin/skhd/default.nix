9cbc139dcf3573980221d4f9dc25641b6c2acfbe
Franck Rasolo <franckrasolo@users.noreply.github.com>
2022-07-17 23:08:24 +0100

    initial skhd configuration for launchers

---
+++ darwin/skhd/default.nix
@@ -0,0 +1,25 @@
+{ pkgs, ... }:
+
+{
+  services.skhd = {
+    enable  = true;
+    package = pkgs.skhd;
+    skhdConfig = let
+      skhdEnv = rec {
+        modMask  = "cmd";
+        moveMask = "cmd + ctrl";
+        noOp     = "/dev/null";
+        prefix   = "yabai -m";
+
+        firstOrSecond = { first, second }: domain:
+          "${prefix} ${domain} --focus ${first} || ${prefix} ${domain} --focus ${second}";
+
+        nextOrFirst = firstOrSecond { first = "next"; second = "first"; };
+        prevOrLast  = firstOrSecond { first = "prev"; second = "last";  };
+      };
+    in
+      with builtins; concatStringsSep "\n" (map (f: import f { inherit skhdEnv; }) [
+        ./launchers.nix
+      ]);
+  };
+}
---
+++ darwin/skhd/launchers.nix
@@ -0,0 +1,15 @@
+{ ... }:
+
+''
+fn - k : open -n -a /Applications/kitty.app --args --directory ~/dev/dotfiles.nix --single-instance
+fn - y : open -n -a /Applications/Alacritty.app --args --config-file ~/.xdg/config/alacritty/alacritty.yml
+fn - 1 : open -a /Applications/1Password\ 7.app
+fn - a : open -a ~/Library/Homebrew/Applications/Alfred\ 4.app
+fn - c : sops -d ~/dev/dotfiles.nix/home/secrets/bootstrap.yaml | sed -n 1p | sed -E 's/opmk: //' | tr -d '\n' | pbcopy
+fn - f : sops -d ~/dev/dotfiles.nix/home/secrets/bootstrap.yaml | sed -n 3p | sed -E 's/s2p2: //' | tr -d '\n' | pbcopy
+fn - l : printf "" | pbcopy
+fn - g : ~/.xdg/local/bin/goland
+fn - i : ~/.xdg/local/bin/idea
+fn - p : open -a /System/Applications/System\ Preferences.app
+fn - z : open -a /Applications/zoom.us.app
+''
