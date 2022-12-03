800e51a9d4615f7a4a8d75e97ea457c3ebcf876b
Franck Rasolo <franckrasolo@users.noreply.github.com>
2022-12-03 16:59:53 +0000

    upgrade nixpkgs to 22.11

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "macOS configurations";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-22.11-darwin";
 
     nix-darwin = {
       url = "github:LnL7/nix-darwin";
