0e314283388c0bdc6be795d25c686e755ec0680f
Franck Rasolo <franckrasolo@users.noreply.github.com>
2023-02-06 00:40:34 +0000

    disable Mission Control hot keys
    https://github.com/LnL7/nix-darwin/issues/518#issuecomment-1291184072
    https://apple.stackexchange.com/questions/344494/how-to-disable-default-mission-control-shortcuts-in-terminal
    https://www.isi.edu/~calvin/mac-missioncontrolshortcuts.htm
    https://zameermanji.com/blog/2021/6/8/applying-com-apple-symbolichotkeys-changes-instantaneously/

--- darwin/macOS/keyboard.nix
+++ darwin/macOS/keyboard.nix
@@ -1,3 +1,5 @@
+{ lib, ... }:
+
 {
   system.keyboard = {
     enableKeyMapping = true;
@@ -18,4 +20,40 @@
 
 #   "com.apple.keyboard.fnState" = false;
   };
+
+  system.activationScripts.extraUserActivation.enable = true;
+  system.activationScripts.extraUserActivation.text = let
+    hotkeys = [
+      32 # Mission Control
+      34
+      33 # Application windows
+      35
+      79 # Move left a space
+      80
+      81 # Move right a space
+      82
+    ];
+    disableHotKeyCommands = map (key:
+      "defaults write com.apple.symbolichotkeys AppleSymbolicHotKeys -dict-add ${toString key} '
+<dict>
+  <key>enabled</key><false/>
+  <key>value</key>
+  <dict>
+    <key>type</key><string>standard</string>
+    <key>parameters</key>
+    <array>
+      <integer>65535</integer>
+      <integer>65535</integer>
+      <integer>0</integer>
+    </array>
+  </dict>
+</dict>'") hotkeys;
+  in ''
+    echo >&2 "configuring hotkeys..."
+
+    ${lib.concatStringsSep "\n" disableHotKeyCommands}
+
+    # credit: https://zameermanji.com/blog/2021/6/8/applying-com-apple-symbolichotkeys-changes-instantaneously/
+    /System/Library/PrivateFrameworks/SystemAdministration.framework/Resources/activateSettings -u
+  '';
 }
