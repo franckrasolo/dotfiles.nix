30a08e6dc7ad16abfa4f3c1add0bee566349d736
Franck Rasolo <franckrasolo@users.noreply.github.com>
2023-07-21 13:32:02 +0100

    disable macOS Terminal services for man pages
    On macOS, whenever the Cmd+Shift+A keyboard shortcut is pressed, the "Search man Page Index in Terminal" service is annoyingly launched together with the "Find Action..." action in JetBrains IDEs.
    
    Disabling this Terminal service fixes this specific issue.
    
    credit: https://apple.stackexchange.com/a/454359

--- darwin/macOS/keyboard.nix
+++ darwin/macOS/keyboard.nix
@@ -33,6 +33,7 @@
       81 # Move right a space
       82
     ];
+
     disableHotKeyCommands = map (key:
       "defaults write com.apple.symbolichotkeys AppleSymbolicHotKeys -dict-add ${toString key} '
 <dict>
@@ -48,6 +49,24 @@
     </array>
   </dict>
 </dict>'") hotkeys;
+
+    terminalServices = [
+      "com.apple.Terminal - Open man Page in Terminal - openManPage"
+      "com.apple.Terminal - Search man Page Index in Terminal - searchManPages"
+    ];
+
+    # credit: https://apple.stackexchange.com/a/454359
+    disableTerminalServicesCommands = map (key:
+      "defaults write pbs NSServicesStatus -dict-add '${key}' '
+<dict>
+  <key>enabled_context_menu</key><false/>
+  <key>enabled_services_menu</key><false/>
+  <key>presentation_modes</key>
+  <dict>
+    <key>ContextMenu</key><false/>
+    <key>ServicesMenu</key><false/>
+  </dict>
+</dict>'") terminalServices;
   in ''
     echo >&2 "configuring hotkeys..."
 
@@ -55,5 +74,9 @@
 
     # credit: https://zameermanji.com/blog/2021/6/8/applying-com-apple-symbolichotkeys-changes-instantaneously/
     /System/Library/PrivateFrameworks/SystemAdministration.framework/Resources/activateSettings -u
+
+    echo >&2 "disabling Terminal services..."
+
+    ${lib.concatStringsSep "\n" disableTerminalServicesCommands}
   '';
 }
