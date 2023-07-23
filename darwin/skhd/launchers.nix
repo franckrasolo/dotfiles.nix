763260d82d6399b54507ece48ea8f5b23774aec8
Franck Rasolo <franckrasolo@users.noreply.github.com>
2023-07-23 18:52:35 +0100

    set WezTerm as the external terminal for macOS

--- darwin/skhd/launchers.nix
+++ darwin/skhd/launchers.nix
@@ -3,7 +3,7 @@
 ''
 fn - k : open -n -a /Applications/kitty.app --args --directory ~/dev/dotfiles.nix --single-instance
 fn - y : open -n -a /Applications/Alacritty.app --args --config-file ~/.xdg/config/alacritty/alacritty.yml
-fn - w : open -n -a ~/Library/Homebrew/Applications/WezTerm.app
+fn - w : open -n -a /opt/homebrew/bin/wezterm
 fn - 1 : open -a /Applications/1Password.app
 fn - a : open -a /Applications/Alfred\ 5.app
 fn - c : sops -d ~/dev/dotfiles.nix/secrets/bootstrap.yaml | sed -n 1p | sed -E 's/opmk: //' | tr -d '\n' | pbcopy
