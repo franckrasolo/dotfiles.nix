006f3f00f07c1adc3112a42627ce175bb71a8210
Franck Rasolo <franckrasolo@users.noreply.github.com>
2023-09-13 15:00:07 +0100

    reassign launcher

--- darwin/skhd/launchers.nix
+++ darwin/skhd/launchers.nix
@@ -7,7 +7,7 @@ lcmd + lshift - 1 : open -a /Applications/1Password.app
 lcmd + lshift - a : open -a /Applications/Alfred\ 5.app
 lcmd + lshift - c : sops -d ~/dev/dotfiles.nix/secrets/bootstrap.yaml | sed -n 1p | sed -E 's/opmk: //' | tr -d '\n' | pbcopy
 lcmd + lshift - f : sops -d ~/dev/dotfiles.nix/secrets/bootstrap.yaml | sed -n 3p | sed -E 's/s2p2: //' | tr -d '\n' | pbcopy
-lcmd + lshift - l : printf "" | pbcopy
+lcmd + lshift - d : printf "" | pbcopy
 lcmd + lshift - g : ~/.xdg/local/bin/goland
 lcmd + lshift - i : ~/.xdg/local/bin/idea
 lcmd + lshift - s : open -a /System/Applications/System\ Settings.app
