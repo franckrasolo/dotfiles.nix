7433f39ee7cdb87f37a4ffd509c44c10448368d5
Franck Rasolo <franckrasolo@users.noreply.github.com>
2023-09-24 23:34:07 +0100

    workaround for defining direnv aliases
    https://github.com/direnv/direnv/issues/73#issuecomment-1192448475
    https://github.com/direnv/direnv/issues/73#issuecomment-1242969787

--- home/direnv.nix
+++ home/direnv.nix
@@ -11,5 +11,29 @@
         warn_timeout = "3s";
       };
     };
+    stdlib = ''
+    # adapted from https://github.com/direnv/direnv/issues/73#issuecomment-1192448475
+
+    export_alias() {
+      local name=$1
+      shift
+      local alias_dir=$PWD/.direnv/aliases
+      local target="$alias_dir/$name"
+      local oldpath="$PATH"
+
+      mkdir -p "$alias_dir"
+      if ! [[ ":$PATH:" == *":$alias_dir:"* ]]; then
+        PATH_add "$alias_dir"
+      fi
+
+      cat << EOF > "$target"
+    #!/usr/bin/env bash
+    PATH="$oldpath"
+
+    $@
+    EOF
+      chmod +x "$target"
+    }
+    '';
   };
 }
