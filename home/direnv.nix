1444bb2004fee6ffd89f8507f38404c1ed5c6d2f
Franck Rasolo <franckrasolo@users.noreply.github.com>
2023-11-02 19:16:58 +0000

    disable direnv's strict_env due to runtime errors in mojo-playground.nix
    modular: error: could not find the installed package path in the config
    /nix/store/h7pjv77v329xc49dg56lm1fxvd8s49gs-nix-direnv-2.3.0/share/nix-direnv/direnvrc:238: pop_var_context: head of shell_variables not a function context
    environment:1073: pop_var_context: head of shell_variables not a function context
    ./.envrc:32: pop_var_context: head of shell_variables not a function context
    direnv: error exit status 1

--- home/direnv.nix
+++ home/direnv.nix
@@ -9,7 +9,7 @@
       global = {
         disable_stdin = false;
         load_dotenv = true;
-        strict_env = true;
+        strict_env = false;
         warn_timeout = "3s";
       };
       whitelist = {
