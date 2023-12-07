3121344993ecd667965487a0819f8afec21893e6
Franck Rasolo <franckrasolo@users.noreply.github.com>
2023-12-07 20:44:02 +0000

    upgrade nixpkgs to 23.11
    https://nixos.org/blog/announcements.html#nixos-23.11
    
    Flake lock file updates:
    
    • Updated input 'home-manager':
        'github:nix-community/home-manager/28535c3a34d79071f2ccb68671971ce0c0984d7e' (2023-11-19)
      → 'github:nix-community/home-manager/aeb2232d7a32530d3448318790534d196bf9427a' (2023-11-24)
    • Updated input 'nixpkgs':
        'github:NixOS/nixpkgs/25e67be8787d1e8d4fbeb7d3d7cf3555718c7143' (2023-12-03)
      → 'github:NixOS/nixpkgs/1ed863a65c91f77e027955818bf6679f4c966320' (2023-12-07)
    • Updated input 'nixpkgs-unstable':
        'github:NixOS/nixpkgs/e92039b55bcd58469325ded85d4f58dd5a4eaf58' (2023-11-29)
      → 'github:NixOS/nixpkgs/2c7f3c0fb7c08a0814627611d9d7d45ab6d75335' (2023-12-04)
    • Updated input 'sops-nix':
        'github:Mic92/sops-nix/8bca48cb9a12bbd8766f359ad00336924e91b7f7' (2023-12-03)
      → 'github:Mic92/sops-nix/e91ece6d2cf5a0ae729796b8f0dedceab5107c3d' (2023-12-04)

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "macOS configurations";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-23.05-darwin";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-23.11-darwin";
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
 
     nix-darwin = {
@@ -11,7 +11,7 @@
     };
 
     home-manager = {
-      url = "github:nix-community/home-manager/release-23.05";
+      url = "github:nix-community/home-manager/release-23.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
