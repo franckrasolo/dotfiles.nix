4c7ccfbb8df372fb68d2a344ff8c499795f7bf65
Franck Rasolo <franckrasolo@users.noreply.github.com>
2024-07-18 13:34:26 +0100

    home-manager packages for general use not related to work

---
+++ home/packages.nix
@@ -0,0 +1,139 @@
+{ pkgs, ... }:
+
+with pkgs.unstable;
+let
+  securityTools = [
+    _1password
+    age
+    gnupg
+    openssl
+    sops
+  ];
+
+  coreTools = [
+    cachix
+    coreutils
+    devenv
+    moreutils
+
+    delta
+    dos2unix
+    dua
+    eza
+    fd
+    fzf
+    glow
+#   gnused
+    go-task
+    gzip
+    hyperfine
+    just
+    lsd
+    navi
+    pkgs.ripgrep
+    pkgs.ripgrep-all
+    sd
+    skim
+    slides
+    stderred # legacyPackages.x86_64-darwin.stderred
+    unrar
+    unzip
+    tree
+    viu
+    watch
+    watchexec
+    xz
+    zellij
+    zip
+    zoxide
+  ];
+
+  systemTools = [
+    bottom
+    gdu
+    htop
+    neofetch
+    nushell
+    pueue
+
+    lf
+    localsend
+    socat
+    vifm
+    watchman
+    xplr
+    yazi
+  ];
+
+  gitTools = [
+    git
+    git-crypt
+    diff-so-fancy
+    lazygit
+  ];
+
+  httpTools = [
+    aria2
+    xh
+  ];
+
+  languageTools = [
+    gettext
+    grex
+    rlwrap
+
+    kotlin
+    luajit
+    luajitPackages.luarocks
+
+    helix
+    neovim
+    neovim-remote
+    tree-sitter
+  ];
+
+  pdfTools = [
+    poppler
+    zathura
+  ];
+
+  pythonTools = [
+    basedpyright
+    black
+    pdm
+    pur
+    ruff
+
+    python312
+#    (python312.withPackages (pkgs: with pkgs; [ pip pynvim ]))
+  ];
+
+  dataTools = [
+    fq
+    ijq
+    jnv
+    jq
+    yq-go
+  ];
+
+  graphicalTools = [
+    graphviz
+    plantuml
+    timg
+  ];
+in
+{
+  home.packages = []
+    ++ securityTools
+    ++ coreTools
+    ++ systemTools
+    ++ gitTools
+    ++ httpTools
+    ++ languageTools
+    ++ pdfTools
+    ++ pythonTools
+    ++ dataTools
+    ++ graphicalTools
+    ++ [ haxor-news ]
+    ;
+}
