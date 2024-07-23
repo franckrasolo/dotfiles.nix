1c1d1ab4d12f03ab3ee276d8caeb8ee4e7c9f54f
Franck Rasolo <franckrasolo@users.noreply.github.com>
2024-07-24 00:04:22 +0100

    improve code navigation with SmiteshP/nvim-navbuddy

--- home/nvim/lua/plugins/navigation-bar.lua
+++ home/nvim/lua/plugins/navigation-bar.lua
@@ -31,5 +31,34 @@ return {
         end,
       })
     end,
+  },
+  {
+    "SmiteshP/nvim-navbuddy",
+    dependencies = {
+      "neovim/nvim-lspconfig",
+      "SmiteshP/nvim-navic",
+      "MunifTanjim/nui.nvim",
+      "numToStr/Comment.nvim",
+      "nvim-telescope/telescope.nvim",
+    },
+    keys = {
+      { "<leader>nv", "<cmd>Navbuddy<cr>", desc = "Nav" },
+    },
+    config = function()
+      local actions = require("nvim-navbuddy.actions")
+      local navbuddy = require("nvim-navbuddy")
+      navbuddy.setup({
+        window = {
+          border = "double",
+        },
+        mappings = {
+          ["j"] = actions.next_sibling(),
+          ["k"] = actions.previous_sibling(),
+          ["h"] = actions.parent(),
+          ["l"] = actions.children(),
+        },
+        lsp = { auto_attach = true },
+      })
+    end,
   }
 }
