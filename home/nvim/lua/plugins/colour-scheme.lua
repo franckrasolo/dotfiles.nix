53f87d3fc0f86dfeaba675fbd8036bb47bbbbb27
Franck Rasolo <franckrasolo@users.noreply.github.com>
2024-07-23 02:39:05 +0100

    initial configuration for marko-cerovac/material.nvim

---
+++ home/nvim/lua/plugins/colour-scheme.lua
@@ -0,0 +1,55 @@
+return {
+  {
+    "marko-cerovac/material.nvim",
+    config = function()
+      require("lualine").setup {
+        options = { theme = "material" }
+      }
+
+      require("material").setup {
+        high_visibility = {
+          darker = true
+        },
+        disable = {
+          background  = false, -- use the terminal background instead
+          term_colors = false, -- do not change terminal colours
+          eob_lines   = false  -- hide end-of-buffer lines
+        },
+
+        custom_colors = function(colors)
+          colors.editor.bg = "#000000"
+          colors.editor.active = colors.editor.bg
+          colors.editor.disabled = colors.editor.selection -- "#090A0C"
+          colors.editor.accent = colors.main.darkyellow
+
+          colors.backgrounds.sidebars = colors.editor.bg
+          colors.backgrounds.floating_windows = colors.editor.bg
+          --colors.backgrounds.non_current_windows = colors.editor.bg_alt
+          colors.backgrounds.non_current_windows = colors.editor.bg
+        end,
+
+        plugins = {
+           "gitsigns",
+           "indent-blankline",
+           "nvim-cmp",
+           "nvim-navic",
+           "nvim-web-devicons",
+           "telescope",
+           "trouble",
+           "which-key",
+        },
+
+        lualine_style = "stealth",
+      }
+
+      vim.g.material_style = "deep ocean"
+      vim.cmd("colorscheme material")
+    end
+  },
+  {
+    "bluz71/vim-moonfly-colors",
+    name = "moonfly",
+    lazy = true,
+    priority = 1000,
+  },
+}
