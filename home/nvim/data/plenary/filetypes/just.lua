8a7527a95031da19fc381cecec921e94758cea76
Franck Rasolo <franckrasolo@users.noreply.github.com>
2024-07-24 01:05:22 +0100

    configure the filetype for Just files
    https://github.com/casey/just

---
+++ home/nvim/data/plenary/filetypes/just.lua
@@ -0,0 +1,12 @@
+return {
+  extension = {
+    ["just"] = "just",
+  },
+  file_name = {
+    [".justfile"] = "just",
+    ["justfile"]  = "just",
+  },
+  shebang = {
+    ["/usr/bin/env just"] = "just",
+  }
+}
