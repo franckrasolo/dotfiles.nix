3710d21d66a57e8aa81ec9d9f66f63c3e0036fca
Franck Rasolo <franckrasolo@users.noreply.github.com>
2024-07-26 12:59:28 +0100

    initial configuration  for HiPhish/rainbow-delimiters.nvim

---
+++ home/nvim/lua/plugins/rainbow-delimiters.lua
@@ -0,0 +1,33 @@
+return {
+  "HiPhish/rainbow-delimiters.nvim",
+  event = "VeryLazy",
+  config = function()
+    local rainbow_delimiters = require("rainbow-delimiters")
+    vim.g.rainbow_delimiters = {
+      strategy = {
+        [""] = rainbow_delimiters.strategy["global"],
+        vim  = rainbow_delimiters.strategy["local"],
+      },
+      query = {
+        [""] = "rainbow-delimiters",
+        lua  = "rainbow-blocks",
+      },
+      priority = {
+        [""] = 110,
+        lua  = 210,
+      },
+      highlight = {
+        "RainbowDelimiterRed",
+        "RainbowDelimiterYellow",
+        "RainbowDelimiterBlue",
+        "RainbowDelimiterOrange",
+        "RainbowDelimiterGreen",
+        "RainbowDelimiterViolet",
+        "RainbowDelimiterCyan",
+      },
+    }
+  end,
+  keys = {
+    { "<leader>ur", function() require("rainbow-delimiters").toggle(0) end, desc = "Toggle Rainbow Delimiters" },
+  },
+}
