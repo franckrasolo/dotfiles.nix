a70177606ffcff642a575a7029208108a73415f9
Franck Rasolo <franckrasolo@users.noreply.github.com>
2024-07-31 15:49:21 +0100

    install Hammerspoon's command line tool

--- darwin/hammerspoon/init.lua
+++ darwin/hammerspoon/init.lua
@@ -2,3 +2,6 @@
 local luarocks = "/etc/profiles/per-user/" .. os.getenv("USER") .. "/bin/luarocks"
 package.path  = package.path  .. ";" .. hs.execute(luarocks .. " path --lr-path"):gsub("\n", "")
 package.cpath = package.cpath .. ";" .. hs.execute(luarocks .. " path --lr-cpath"):gsub("\n", "")
+
+-- install Hammerspoon's command line tool as /opt/homebrew/bin/hs
+hs.ipc.cliInstall()
