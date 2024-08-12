8c599852e7faf9f0c781a6fd439f5bea8e1de1bc
Franck Rasolo <franckrasolo@users.noreply.github.com>
2024-08-12 19:08:01 +0100

    apply the Aerospace suggestion of grouping windows by application
    https://nikitabobko.github.io/AeroSpace/guide#a-note-on-mission-control

--- darwin/macOS/spaces.nix
+++ darwin/macOS/spaces.nix
@@ -1,4 +1,8 @@
 {
+  # group windows by application in Mission Control's Exposé
+  # see https://nikitabobko.github.io/AeroSpace/guide#a-note-on-mission-control
+  system.defaults.dock.expose-group-by-app = true;
+
   # do not automatically rearrange Spaces based on most recent use
   system.defaults.dock.mru-spaces = false;
 
