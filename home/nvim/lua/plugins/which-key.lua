66006b2be498a29968fde81ec0b9ef32ad9d333d
Franck Rasolo <franckrasolo@users.noreply.github.com>
2024-08-26 08:18:48 +0100

    configure folke/which-key.nvim

---
+++ home/nvim/lua/plugins/which-key.lua
@@ -0,0 +1,16 @@
+return {
+  "folke/which-key.nvim",
+  opts = {
+    preset = "modern",
+    win = {
+      -- allow the popup to overlap with the cursor so that options are more easily discoverable
+      no_overlap = false,
+      padding = { 1, 1 }, -- padding: { top/bottom, right/left }
+      title = true,
+      title_pos = "center",
+    },
+    layout = {
+      spacing = 1, -- spacing between columns
+    },
+  }
+}
