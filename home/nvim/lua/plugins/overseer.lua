cf18feb041225fe115d901952cb01f6781ce978d
Franck Rasolo <franckrasolo@users.noreply.github.com>
2024-08-26 08:29:48 +0100

    configure stevearc/overseer.nvim
    already installed through 'lazyvim.plugins.extras.editor.overseer'

---
+++ home/nvim/lua/plugins/overseer.lua
@@ -0,0 +1,38 @@
+return {
+  "stevearc/overseer.nvim",
+  opts = {
+    strategy = {
+      "toggleterm",
+      use_shell = false,
+      direction = "horizontal",
+      open_on_start = false,
+      -- command to run with `use_shell` when the terminal is created and before starting the task
+      on_create = nil,
+      auto_scroll = false,
+      close_on_exit = false,
+      -- close the toggleterm window without deleting the terminal buffer
+      -- only if the successful task's exit code is 0
+      quit_on_exit = "success",
+      -- mirrors the toggleterm "hidden" parameter, and keeps the task from
+      -- being rendered in the toggleable window
+      hidden = false,
+      highlights = nil,
+    },
+    templates = {
+      "builtin",
+    },
+    task_list = {
+      default_detail = 2,
+      direction = "right",
+      min_width = { 20, 0.25 },
+      bindings = {
+        ["="] = "IncreaseDetail",
+        ["-"] = "DecreaseDetail",
+        ["<C-=>"] = "IncreaseAllDetail",
+        ["<C-->"] = "DecreaseAllDetail",
+        ["<C-u>"] = "ScrollOutputUp",
+        ["<C-d>"] = "ScrollOutputDown",
+      },
+    },
+  },
+}
