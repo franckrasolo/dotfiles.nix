bec2b932cffe402fa60d1bf2f613d37f5423f91f
Franck Rasolo <franckrasolo@users.noreply.github.com>
2024-09-02 22:55:17 +0100

    move WezTerm configuration modules

--- home/wezterm/wezterm.lua
+++ home/wezterm/wezterm.lua
@@ -3,9 +3,9 @@ config:set_strict_mode(true)
 config.automatically_reload_config = true
 config.scrollback_lines = 100000
 
-require("appearance")(config)
-require("key_bindings")(config)
-require("typography")(config)
+require("config.appearance")(config)
+require("config.key_bindings")(config)
+require("config.typography")(config)
 require("plugins.tabline")(config)
 
 require("events.gui-startup.adjust_font_window_sizes")
