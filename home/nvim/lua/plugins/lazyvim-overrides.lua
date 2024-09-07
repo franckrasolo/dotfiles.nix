9e54d60b7e32c8f806d8700e0ff5fedc5b519071
Franck Rasolo <franckrasolo@users.noreply.github.com>
2024-09-07 14:42:55 +0100

    group the rustaceanvim upgrade with other LazyVim/Extras overrides

--- home/nvim/lua/plugins/lazyvim-overrides.lua
+++ home/nvim/lua/plugins/lazyvim-overrides.lua
@@ -1,4 +1,5 @@
 return {
   { "akinsho/bufferline.nvim", enabled = false },
   { "nvim-neo-tree/neo-tree.nvim", enabled = false },
+  { "mrcjkb/rustaceanvim", version = "^5", --[[recommended]] },
 }
