13c2951d394f538806413db4fa2352443831b6ec
Franck Rasolo <franckrasolo@users.noreply.github.com>
2024-09-07 20:44:48 +0100

    use the latest release of mistweaverco/kulala.nvim
    This is to reduce the frequency of updates.

--- home/nvim/lua/plugins/lazyvim-overrides.lua
+++ home/nvim/lua/plugins/lazyvim-overrides.lua
@@ -1,5 +1,7 @@
 return {
   { "akinsho/bufferline.nvim", enabled = false },
   { "nvim-neo-tree/neo-tree.nvim", enabled = false },
+
+  { "mistweaverco/kulala.nvim", version = "*" --[[latest release]] },
   { "mrcjkb/rustaceanvim", version = "^5", --[[recommended]] },
 }
