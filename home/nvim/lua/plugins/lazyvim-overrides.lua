6cfe0cfce354c7d2166d5d2b0f23973a4553e6f5
Franck Rasolo <franckrasolo@users.noreply.github.com>
2024-09-08 06:04:44 +0100

    prevent rust-analyzer from following symlinks to the Nix store
    https://www.reddit.com/r/rust/comments/12cn0gz/comment/jf308vu/

--- home/nvim/lua/plugins/lazyvim-overrides.lua
+++ home/nvim/lua/plugins/lazyvim-overrides.lua
@@ -3,5 +3,30 @@ return {
   { "nvim-neo-tree/neo-tree.nvim", enabled = false },
 
   { "mistweaverco/kulala.nvim", version = "*" --[[latest release]] },
-  { "mrcjkb/rustaceanvim", version = "^5", --[[recommended]] },
+  {
+    "mrcjkb/rustaceanvim",
+    version = "^5", -- recommended
+    opts = {
+      server = {
+        default_settings = {
+          ["rust-analyzer"] = {
+            files = {
+              -- would not be required if rust-analyzer honored .gitignore
+              excludeDirs = {
+                ".direnv",
+                ".git",
+                ".github",
+                ".gitlab",
+                ".idea",
+                "bin",
+                "node_modules",
+                "target",
+                "venv",
+              }
+            }
+          }
+        }
+      }
+    }
+  },
 }
