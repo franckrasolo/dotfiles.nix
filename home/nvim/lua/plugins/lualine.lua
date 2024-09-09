85363e50c6e149cc73b41cda5eb5d144b3d9fa0f
Franck Rasolo <franckrasolo@users.noreply.github.com>
2024-09-09 17:01:35 +0100

    configure lualine.nvim for the catppuccin colour scheme

--- home/nvim/lua/plugins/lualine.lua
+++ home/nvim/lua/plugins/lualine.lua
@@ -1,20 +1,39 @@
 return {
   "nvim-lualine/lualine.nvim",
-  config = function()
-    require("lualine").setup {
-      options = {
-        icons_enabled = true,
-        theme = "onedark",
-        component_separators = "|",
-        section_separators = "",
+  dependencies = { "catppuccin/nvim" },
+  config = function(_, opts)
+    table.remove(opts.sections.lualine_c)
+    require("lualine").setup(opts)
+  end,
+  opts = {
+    options = {
+      icons_enabled = true,
+      component_separators = { left = "", right = "" },
+      section_separators = { left = "", right = "" },
+    },
+    theme = "catppuccin",
+    sections = {
+      lualine_a = { { "mode", icon = "", padding = { left = 1, right = 0 } } },
+      lualine_b = {},
+      lualine_c = {
+        LazyVim.lualine.root_dir(),
+        {
+          "diagnostics",
+          symbols = {
+            error = LazyVim.config.icons.diagnostics.Error,
+            warn = LazyVim.config.icons.diagnostics.Warn,
+            info = LazyVim.config.icons.diagnostics.Info,
+            hint = LazyVim.config.icons.diagnostics.Hint,
+          },
+        },
+        { "filetype", icon_only = true, separator = "", padding = { left = 1, right = 0 } },
+        { LazyVim.lualine.pretty_path() },
       },
-      sections = {
-        lualine_a = {
-          {
-            "buffers",
-          }
-        }
-      }
-    }
-  end
+      lualine_y = { { "branch", padding = { left = 0, right = 1 } }, "encoding" },
+      lualine_z = {
+        { "location", padding = { left = 1, right = 1 } },
+        { "progress", padding = { left = 1, right = 1 } },
+      },
+    },
+  }
 }
