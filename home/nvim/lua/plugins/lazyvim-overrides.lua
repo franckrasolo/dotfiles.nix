69fb95de1b02dd1c0ae9e8cf10ad6748b1660a43
Franck Rasolo <franckrasolo@users.noreply.github.com>
2024-09-10 13:30:19 +0100

    load yanky.nvim lazily

--- home/nvim/lua/plugins/lazyvim-overrides.lua
+++ home/nvim/lua/plugins/lazyvim-overrides.lua
@@ -1,6 +1,7 @@
 return {
   { "akinsho/bufferline.nvim", enabled = false },
   { "nvim-neo-tree/neo-tree.nvim", enabled = false },
+  { "gbprod/yanky.nvim", lazy = true },
 
   { "mistweaverco/kulala.nvim", version = "*" --[[latest release]] },
   {
