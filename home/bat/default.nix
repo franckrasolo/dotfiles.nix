e442db60dd47233eab64312fc2131da074fd3038
Franck Rasolo <franckrasolo@users.noreply.github.com>
2024-09-11 12:53:01 +0100

    rewrite the Nix module for sharkdp/bat as a Home Manager module
    https://home-manager-options.extranix.com/?query=programs.bat

--- home/bat/default.nix
+++ home/bat/default.nix
@@ -1,13 +1,39 @@
 { pkgs, lib, ... }:
 
 {
-  home.packages = [ pkgs.unstable.bat ];
-
-  xdg.configFile."bat/config".source  = pkgs.writeText "config" ''
-    --map-syntax="*{j,J}ustfile*:Makefile"
-    --pager="less -FR"
-    --theme="gruvbox-dark"
-  '';
+  programs.bat = {
+    enable = true;
+    package = pkgs.unstable.bat;
+    config = {
+      map-syntax = [
+        "*{j,J}ustfile*:Just"
+      ];
+      pager = "less -FR";
+      theme = "Catppuccin Mocha";
+    };
+    syntaxes = {
+      Just = {
+        src = pkgs.fetchFromGitHub {
+          owner = "nk9";
+          repo = "just_sublime";
+          rev = "08bbc62e9e77c82fb0fa6cabc0630cb5cc4bcd0e";
+          hash = "sha256-wSZe0uklnH3SooFR8RqAeGj3WL3W2cqNeSH/nQS3/4s=";
+        };
+        file = "Syntax/Just.sublime-syntax";
+      };
+    };
+    themes = {
+      "Catppuccin Mocha" = {
+        src = pkgs.fetchFromGitHub {
+          owner = "catppuccin";
+          repo = "bat";
+          rev = "d714cc1d358ea51bfc02550dabab693f70cccea0";
+          hash = "sha256-Q5B4NDrfCIK3UAMs94vdXnR42k4AXCqZz6sRn8bzmf4=";
+        };
+        file = "themes/Catppuccin Mocha.tmTheme";
+      };
+    };
+  };
 
   home.activation.batCache = lib.hm.dag.entryAfter [ "linkGeneration" ] ''
     $VERBOSE_ECHO "Rebuilding the bat theme cache"
