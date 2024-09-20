c81c1b8be3fa87b790cffaa0c21436141833d482
Franck Rasolo <franckrasolo@users.noreply.github.com>
2024-09-20 11:12:32 +0100

    override mini.move keymaps

--- home/nvim/lua/plugins/lazyvim-overrides.lua
+++ home/nvim/lua/plugins/lazyvim-overrides.lua
@@ -4,6 +4,24 @@ return {
   { "gbprod/yanky.nvim", lazy = true },
 
   {
+    "echasnovski/mini.move",
+    opts = {
+      mappings = {
+        -- move visual selection in visual mode
+        left = "<S-Left>",
+        right = "<S-Right>",
+        down = "<S-Down>",
+        up = "<S-Up>",
+
+        -- move current line in normal mode
+        line_left = "<S-Left>",
+        line_right = "<S-Right>",
+        line_down = "<S-Down>",
+        line_up = "<S-Up>",
+      },
+    },
+  },
+  {
     "mistweaverco/kulala.nvim",
     version = "*", --latest release
     opts = {
