49ef1032f85a50c23215722d419f79e555dc85dc
Franck Rasolo <franckrasolo@users.noreply.github.com>
2024-09-24 22:07:55 +0200

    delay loading of the emoji extension for Telescope

--- home/nvim/lua/plugins/emojis.lua
+++ home/nvim/lua/plugins/emojis.lua
@@ -1,11 +1,10 @@
 return {
   "xiyaowong/telescope-emoji.nvim",
-  event = "VeryLazy",
   dependencies = { "nvim-telescope/telescope.nvim" },
-  init = function()
-		require("telescope").load_extension("emoji")
-	end,
-  keys =  {
+  config = function()
+    require("telescope").load_extension("emoji")
+  end,
+  keys = {
     { "<leader>se", desc = "Insert an emoji", "<cmd>Telescope emoji<cr>" },
   }
 }
