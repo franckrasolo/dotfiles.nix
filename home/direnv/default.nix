e1d060d776a2e337f8fd5cfe7c7d22dc15f468e9
Franck Rasolo <franckrasolo@users.noreply.github.com>
2024-09-25 02:45:38 +0100

    extract additional functions for direnv

--- home/direnv/default.nix
+++ home/direnv/default.nix
@@ -27,29 +27,6 @@
         ];
       };
     };
-    stdlib = ''
-    # adapted from https://github.com/direnv/direnv/issues/73#issuecomment-1192448475
-
-    export_alias() {
-      local name=$1
-      shift
-      local alias_dir=$PWD/.direnv/aliases
-      local target="$alias_dir/$name"
-      local oldpath="$PATH"
-
-      mkdir -p "$alias_dir"
-      if ! [[ ":$PATH:" == *":$alias_dir:"* ]]; then
-        PATH_add "$alias_dir"
-      fi
-
-      cat << EOF > "$target"
-    #!/usr/bin/env bash
-    PATH="$oldpath"
-
-    $@
-    EOF
-      chmod +x "$target"
-    }
-    '';
+    stdlib = builtins.readFile ./direnvrc;
   };
 }
