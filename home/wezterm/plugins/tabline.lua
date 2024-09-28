d769001b42a6d9dadc871e3fb2d3cd4ef3e3af12
Franck Rasolo <franckrasolo@users.noreply.github.com>
2024-09-28 08:48:22 +0200

    reconfigure tabline.wez to include process icons

--- home/wezterm/plugins/tabline.lua
+++ home/wezterm/plugins/tabline.lua
@@ -1,8 +1,14 @@
 local wezterm = require("wezterm")
+local accent_colors = wezterm.color.get_builtin_schemes()["Catppuccin Mocha"]
+
+local process_to_icon = {
+  ["yazi"] = { wezterm.nerdfonts.md_duck, color = { fg = accent_colors.ansi[5] } },
+  ["zellij"] = { wezterm.nerdfonts.md_hexagon_slice_6, color = { fg = accent_colors.ansi[4] } },
+  ["zsh"] = { wezterm.nerdfonts.oct_terminal, color = { fg = "#A4A9BB" } },
+}
 
 return function(config)
   local tabline = wezterm.plugin.require("https://github.com/michaelbrusegard/tabline.wez")
-  local accent_colors = wezterm.color.get_builtin_schemes()["Catppuccin Mocha"]
 
   local function icon()
     return " 󰰮 "
@@ -41,21 +47,28 @@ return function(config)
       tabline_b = { "mode" },
       tabline_c = { " " },
       tab_active = {
-        { Attribute = { Intensity = "Bold" } },
         { Foreground = { Color = accent_colors.ansi[3] } },
-        "tab_index",
-        "ResetAttributes",
+        "index",
         { Foreground = { Color = accent_colors.foreground } },
         { "parent", max_length = 20, padding = 0 },
         "/",
-        { Attribute = { Intensity = "Bold" } },
         { "cwd", max_length = 20, padding = { left = 0, right = 1 } },
+        { Foreground = { Color = accent_colors.ansi[3] } },
+        { Text = wezterm.nerdfonts.ple_forwardslash_separator },
+        { "process", icons_only = true, process_to_icon = process_to_icon, padding = { left = 1, right = 0 } },
       },
       tab_inactive = {
         { Foreground = { Color = accent_colors.ansi[6] } },
-        "tab_index",
-        "ResetAttributes",
-        { "process", padding = { left = 0, right = 1 } }
+        "index",
+        { Foreground = { Color = "#727272" } },
+        { "parent", max_length = 20, padding = 0 },
+        "/",
+        { "cwd", max_length = 20, padding = { left = 0, right = 1 } },
+        { Foreground = { Color = accent_colors.ansi[6] } },
+        { Text = wezterm.nerdfonts.ple_forwardslash_separator },
+        { Background = { Color = accent_colors.tab_bar.background } },
+        { Foreground = { Color = "#727272" } },
+        { "process", icons_only = true, process_to_icon = process_to_icon, padding = { left = 1, right = 0 } },
       },
       tabline_x = {},
       tabline_y = { { "datetime", style = "%a %d %b %Y %H:%M:%S", hour_to_icon = "" }, "battery" },
@@ -63,9 +76,4 @@ return function(config)
     },
     extensions = {},
   }
-
-  config.colors["tab_bar"] = {
-    background = require("tabline.config").colors.normal_mode.c.bg,
-  }
-  config.status_update_interval = 250
 end
