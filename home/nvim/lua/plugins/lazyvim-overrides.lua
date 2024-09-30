8a4de066e0e8cfa6457897637790c5493c280546
Franck Rasolo <franckrasolo@users.noreply.github.com>
2024-09-30 16:43:50 +0100

    extract mini.move to plugins/overrides

--- home/nvim/lua/plugins/lazyvim-overrides.lua
+++ home/nvim/lua/plugins/lazyvim-overrides.lua
@@ -1,23 +1,4 @@
 return {
   { "akinsho/bufferline.nvim", enabled = false },
   { "nvim-neo-tree/neo-tree.nvim", enabled = false },
-
-  {
-    "echasnovski/mini.move",
-    opts = {
-      mappings = {
-        -- move visual selection in visual mode
-        left = "<S-Left>",
-        right = "<S-Right>",
-        down = "<S-Down>",
-        up = "<S-Up>",
-
-        -- move current line in normal mode
-        line_left = "<S-Left>",
-        line_right = "<S-Right>",
-        line_down = "<S-Down>",
-        line_up = "<S-Up>",
-      },
-    },
-  },
 }
---
+++ home/nvim/lua/plugins/overrides/mini.move.lua
@@ -0,0 +1,18 @@
+return {
+  "echasnovski/mini.move",
+  opts = {
+    mappings = {
+      -- move visual selection in visual mode
+      left = "<S-Left>",
+      right = "<S-Right>",
+      down = "<S-Down>",
+      up = "<S-Up>",
+
+      -- move current line in normal mode
+      line_left = "<S-Left>",
+      line_right = "<S-Right>",
+      line_down = "<S-Down>",
+      line_up = "<S-Up>",
+    },
+  },
+}
