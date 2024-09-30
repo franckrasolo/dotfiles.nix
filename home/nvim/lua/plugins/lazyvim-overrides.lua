2aeea8c7f97ddc27fdd4c113bfbaf1094d6fd4e7
Franck Rasolo <franckrasolo@users.noreply.github.com>
2024-09-30 16:27:22 +0100

    extract LuaSnip to plugins/overrides

--- home/nvim/lua/plugins/lazyvim-overrides.lua
+++ home/nvim/lua/plugins/lazyvim-overrides.lua
@@ -21,20 +21,6 @@ return {
     },
   },
   {
-    "L3MON4D3/LuaSnip",
-    build = "make install_jsregexp",
-    config = function()
-      -- additional snippets from friendly-snippets
-      local luasnip = require("luasnip")
-      luasnip.filetype_extend("kotlin", { "kdoc" })
-      luasnip.filetype_extend("lua", { "luadoc" })
-      luasnip.filetype_extend("python", { "comprehension", "pydoc" })
-      luasnip.filetype_extend("rust", { "rustdoc" })
-      luasnip.filetype_extend("sh", { "shelldoc" })
-      luasnip.filetype_extend("typescript", { "tsdoc" })
-    end
-  },
-  {
     "mrcjkb/rustaceanvim",
     version = "^5", -- recommended
     opts = {
