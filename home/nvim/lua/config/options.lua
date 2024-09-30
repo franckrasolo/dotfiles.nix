2864a6941b89414d5000c3191dbf60cddd3e3a06
Franck Rasolo <franckrasolo@users.noreply.github.com>
2024-09-30 06:48:12 +0100

    expand languages to spell check in Neovim

--- home/nvim/lua/config/options.lua
+++ home/nvim/lua/config/options.lua
@@ -2,6 +2,8 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
+vim.opt.spelllang = { "en_gb", "en_us", "fr_fr", "pt_br" }
+
 vim.opt.winbar = "%=%m %f" -- shows filename in top right corner
 
 vim.g.autoformat = false   -- do not automatically format on save
