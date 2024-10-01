b40ff1cfc87a6e12937733488492d43e90b801ea
Franck Rasolo <franckrasolo@users.noreply.github.com>
2024-10-01 05:35:22 +0100

    disable mini.ai as it breaks `vi(` when combined with multicursor.nvim
    Alas none of mini.ai's search methods produced the expected function parameter selection discussed in https://github.com/jake-stewart/multicursor.nvim/issues/38.
    
    https://github.com/echasnovski/mini.nvim/issues/790

--- home/nvim/lua/plugins/overrides/misc.lua
+++ home/nvim/lua/plugins/overrides/misc.lua
@@ -1,4 +1,5 @@
 return {
   { "akinsho/bufferline.nvim", enabled = false },
+  { "echasnovski/mini.ai", enabled = false }, -- breaks `vi(` and `vi)` with multicursor.nvim
   { "nvim-neo-tree/neo-tree.nvim", enabled = false },
 }
