0977627e25c3a7797950708ccb8d7ec6009e4243
Franck Rasolo <franckrasolo@users.noreply.github.com>
2024-10-04 05:24:26 +0100

    remove redundant menubar items
    Most of them remain accessible within Control Center.

--- darwin/macOS/menubar.nix
+++ darwin/macOS/menubar.nix
@@ -2,7 +2,23 @@
 
 {
   system.defaults.CustomUserPreferences."~/Library/Preferences/ByHost/com.apple.controlcenter.plist" = {
+    "AirDrop"          = 8;
+    "AudioVideoModule" = 8;
+    "BentoBox"         = 8;
+    "Bluetooth"        = 8;
+    "Display"          = 9;
+    "DoNotDisturb"     = 8;
+    "FocusModes"       = 8;
     "MusicRecognition" = 9;
+    "NowPlaying"       = 8;
+    "Shortcuts"        = 8;
+    "StateManager"     = 8;
+    "Sound"            = 8;
+    "WiFi"             = 8;
+  };
+
+  system.defaults.CustomUserPreferences."~/Library/Preferences/ByHost/com.apple.Spotlight.plist" = {
+    "MenuItemHidden" = true;
   };
 
   system.defaults.menuExtraClock = {
