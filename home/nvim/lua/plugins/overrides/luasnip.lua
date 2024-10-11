0d629b28e1de5bbe12c2f206cf5aeae5e7793180
Franck Rasolo <franckrasolo@users.noreply.github.com>
2024-10-11 16:35:12 +0100

    add keyboard navigation of LuaSnip snippets

--- home/nvim/lua/plugins/overrides/luasnip.lua
+++ home/nvim/lua/plugins/overrides/luasnip.lua
@@ -15,5 +15,31 @@ return {
       paths = { "./lua/snippets" },
       fs_event_providers = { libuv = true },
     }
+
+    local function map(key, fn)
+      vim.keymap.set({ "i", "s" }, key, fn, { silent = true })
+    end
+
+    map("<c-j>", function() if luasnip.expand_or_jumpable() then luasnip.expand_or_jump() end end)
+    map("<c-k>", function() if luasnip.jumpable(-1) then luasnip.jump(-1) end end)
+    map("<c-l>", function() if luasnip.choice_active() then luasnip.change_choice(1) end end)
   end,
+  opts = {
+    -- disable history
+    keep_roots = false,
+    link_roots = false,
+    link_children = false,
+    exit_roots = true,
+
+    -- enable live buffer updates as you type
+    update_events = { "TextChanged", "TextChangedI" },
+
+    ext_opts = {
+      [require("luasnip.util.types").choiceNode] = {
+        active = {
+          virt_text = { { " « Choice", "Comment" } },
+        },
+      },
+    },
+  },
 }
