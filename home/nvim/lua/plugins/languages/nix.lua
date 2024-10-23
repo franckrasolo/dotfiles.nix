0e2f471751446c9537a94c5bea22213526411d72
Franck Rasolo <franckrasolo@users.noreply.github.com>
2024-10-23 23:40:26 +0100

    add nix-darwin options to the Nix language server
    This makes Nix documentation available for nix-darwin options.

--- home/nvim/lua/plugins/languages/nix.lua
+++ home/nvim/lua/plugins/languages/nix.lua
@@ -1,12 +1,23 @@
 return {
   "neovim/nvim-lspconfig",
   config = function()
+    local flake_dir = "~/dev/dotfiles.nix"
+
     require("lspconfig").nixd.setup {
       cmd = { "nixd" },
       settings = {
         nixd = {
           nixpkgs = {
             expr = "import <nixpkgs> {}",
+          },
+          options = {
+            nix_darwin = {
+              expr = string.format(
+                "(builtins.getFlake \"%s\").darwinConfigurations.%s.options",
+                flake_dir,
+                vim.uv.os_gethostname()
+              ),
+            },
           },
         },
       },
