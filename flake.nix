55d550db940c544c6ff9db9ca2f2a9c476207e70
Franck Rasolo <franckrasolo@users.noreply.github.com>
2025-03-13 10:10:21 +0000

    make nix-darwin use nixpkgs-24.11

--- flake.nix
+++ flake.nix
@@ -6,7 +6,7 @@
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
 
     nix-darwin = {
-      url = "github:LnL7/nix-darwin";
+      url = "github:LnL7/nix-darwin/nix-darwin-24.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
