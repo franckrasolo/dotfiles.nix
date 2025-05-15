25cf77784063696d93ef9ddd0812c9607667e498
Franck Rasolo <franckrasolo@users.noreply.github.com>
2025-05-15 11:07:54 +0200

    sync LazyVim settings with https://github.com/LazyVim/starter

--- home/nvim/lua/config/lazy.lua
+++ home/nvim/lua/config/lazy.lua
@@ -33,7 +33,10 @@ require("lazy").setup({
   },
   rocks = { enabled = false, hererocks = true },
   install = { colorscheme = { "tokyonight", "habamax" } },
-  checker = { enabled = true }, -- automatically check for plugin updates
+  checker = {
+    enabled = true, -- check for plugin updates periodically
+    notify = false, -- notify on update
+  }, -- automatically check for plugin updates
   performance = {
     rtp = {
       -- disable some rtp plugins
