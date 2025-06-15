966b64fc13a533850030aa75cdd783f17838b304
Franck Rasolo <franckrasolo@users.noreply.github.com>
2025-06-15 19:34:21 +0200

    align snacks.nvim options with those of its full example

--- home/nvim/lua/plugins/snacks.lua
+++ home/nvim/lua/plugins/snacks.lua
@@ -5,11 +5,18 @@ return {
   ---@type snacks.Config
   opts = {
     bigfile = { enabled = true },
+    dashboard = { enabled = true },
+    explorer = { enabled = true },
+    indent = { enabled = true },
+    input = { enabled = true },
     notifier = {
       enabled = true,
       timeout = 3000,
     },
+    picker = { enabled = true },
     quickfile = { enabled = true },
+    scope = { enabled = true },
+    scroll = { enabled = true },
     statuscolumn = { enabled = true },
     words = { enabled = true },
     styles = {
