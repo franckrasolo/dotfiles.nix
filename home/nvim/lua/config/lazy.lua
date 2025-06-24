4731f6c520dd87cea73900cfdaade533d8292454
Franck Rasolo <franckrasolo@users.noreply.github.com>
2025-06-24 16:04:37 +0100

    enable LazyVim notification of plugin updates

--- home/nvim/lua/config/lazy.lua
+++ home/nvim/lua/config/lazy.lua
@@ -35,7 +35,7 @@ require("lazy").setup({
   install = { colorscheme = { "tokyonight", "habamax" } },
   checker = {
     enabled = true, -- check for plugin updates periodically
-    notify = false, -- notify on update
+    notify = true, -- notify on update
   }, -- automatically check for plugin updates
   performance = {
     rtp = {
