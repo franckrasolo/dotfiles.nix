f1b6bb96f50ebddc60394ed79923d8aa34b9b992
Franck Rasolo <franckrasolo@users.noreply.github.com>
2025-06-29 10:24:33 +0200

    tweak comment

--- home/nvim/lua/plugins/languages/python.lua
+++ home/nvim/lua/plugins/languages/python.lua
@@ -14,7 +14,7 @@ return {
   vim.api.nvim_create_autocmd("LspAttach", {
     callback = function(args)
       local client = vim.lsp.get_client_by_id(args.data.client_id)
-      -- stop ruff clients with a "utf-8" offset encoding
+      -- stop ruff clients with the default "utf-8" offset encoding
       if client and client.name == "ruff" and client.offset_encoding == "utf-8" then
         vim.lsp.stop_client(client.id, true)
       end
