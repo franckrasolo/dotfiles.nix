2598a091aee9e06b285d4ea29fdcb3de5fe1ae80
Franck Rasolo <franckrasolo@users.noreply.github.com>
2025-07-08 19:52:29 +0100

    remove unnecessary SchemaStore overrides

--- home/nvim/after/lsp/yamlls.lua
+++ home/nvim/after/lsp/yamlls.lua
@@ -36,29 +36,8 @@ return {
       schemas = require("schemastore").yaml.schemas {
         extra = {
           {
-            name = "docker-compose",
-            description = "docker-compose YAML schema",
-            url = "https://raw.githubusercontent.com/compose-spec/compose-spec/master/schema/compose-spec.json",
-            fileMatch = {
-              "**/compose.{yaml,yml}",
-              "**/compose.*.{yaml,yml}",
-              "**/docker-compose.{yaml,yml}",
-              "**/docker-compose.*.{yaml,yml}",
-            },
-          },
-          {
-            name = "github-action",
-            description = "github-action YAML schema",
-            url = "https://www.schemastore.org/github-workflow.json",
-            fileMatch = {
-              "*/.github/workflows/*.{yaml,yml}",
-              "**/.gitea/workflows/*.{yaml,yml}",
-              "**/.forgejo/workflows/*.{yaml,yml}",
-            },
-          },
-          {
             name = "gitlab-ci",
-            description = "gitlab-ci YAML schema",
+            description = "GitLab CI schema",
             url = "https://gitlab.com/gitlab-org/gitlab/-/raw/master/app/assets/javascripts/editor/schema/ci.json",
             fileMatch = {
               "**/*.gitlab-ci.{yaml,yml}",
@@ -69,37 +48,11 @@ return {
           },
           {
             name = "kubernetes",
-            description = "Kubernetes YAML schema",
+            description = "Kubernetes schema",
             url = "https://raw.githubusercontent.com/yannh/kubernetes-json-schema/refs/heads/master/v1.33.2-standalone-strict/all.json",
             fileMatch = {
               "**/*.k8s.{yaml,yml}",
-              "**/k8s/*.{yaml,yml}",
-            },
-          },
-          {
-            name = "graphql",
-            description = "GraphQL YAML schema",
-            url = "https://unpkg.com/graphql-config/config-schema.json",
-            fileMatch = {
-              "graphql.config.{yaml,yml}",
-              ".graphqlrc",
-              ".graphqlrc.{yaml,yml}",
-            },
-          },
-          {
-            name = "pre-commit configuration",
-            description = "pre-commit configuration YAML schema",
-            url = "https://www.schemastore.org/pre-commit-config.json",
-            fileMatch = {
-              ".pre-commit-config.{yaml,yml}",
-            },
-          },
-          {
-            name = "pre-commit hooks",
-            description = "pre-commit hooks YAML schema",
-            url = "https://www.schemastore.org/pre-commit-hooks.json",
-            fileMatch = {
-              ".pre-commit-hooks.{yaml,yml}",
+              "**/k8s/**/*.{yaml,yml}",
             },
           },
         },
