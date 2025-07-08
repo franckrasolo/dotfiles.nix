89e4d30866f4e5b15b66a35c020a3958c39e6e0a
Franck Rasolo <franckrasolo@users.noreply.github.com>
2025-07-08 11:01:26 +0100

    stop harper_ls clients without root_dir

--- home/nvim/lua/plugins/languages/harper.lua
+++ home/nvim/lua/plugins/languages/harper.lua
@@ -14,9 +14,11 @@ return {
   vim.api.nvim_create_autocmd("LspAttach", {
     callback = function(args)
       local client = vim.lsp.get_client_by_id(args.data.client_id)
-      -- stop harper_ls clients with empty settings
-      if client and client.name == "harper_ls" and next(client.settings) == nil then
-        vim.lsp.stop_client(client.id, true)
+      if client and client.name == "harper_ls" then
+        -- stop harper_ls clients without root_dir or with empty settings
+        if client.root_dir == nil or next(client.settings) == nil then
+          vim.lsp.stop_client(client.id, true)
+        end
       end
     end
   })
