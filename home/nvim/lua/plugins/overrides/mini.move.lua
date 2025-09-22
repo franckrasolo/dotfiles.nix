4c09c9025f9ca8151a4391734048fc2abf5466f8
Franck Rasolo <franckrasolo@users.noreply.github.com>
2025-09-22 10:53:40 +0100

    update coordinates of  mini plugins to nvim-mini

--- home/nvim/lua/plugins/overrides/mini.move.lua
+++ home/nvim/lua/plugins/overrides/mini.move.lua
@@ -1,5 +1,5 @@
 return {
-  "echasnovski/mini.move",
+  "nvim-mini/mini.move",
   opts = {
     mappings = {
       -- move visual selection in visual mode
--- home/nvim/lua/plugins/overrides/misc.lua
+++ home/nvim/lua/plugins/overrides/misc.lua
@@ -1,6 +1,6 @@
 return {
   { "akinsho/bufferline.nvim", enabled = false },
-  { "echasnovski/mini.ai", enabled = false }, -- breaks `vi(` and `vi)` with multicursor.nvim
+  { "nvim-mini/mini.ai", enabled = false }, -- breaks `vi(` and `vi)` with multicursor.nvim
   { "nvim-neo-tree/neo-tree.nvim", enabled = false },
   { "MeanderingProgrammer/render-markdown.nvim", enabled = false },
   { "linux-cultist/venv-selector.nvim", branch = "main" },
