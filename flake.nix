71bddd1c83392b43ff134de2ade8ce4028603430
Franck Rasolo <franckrasolo@users.noreply.github.com>
2025-11-18 12:27:14 +0000

    use the latest release of flake-checker due to breaking changes

--- flake.nix
+++ flake.nix
@@ -95,7 +95,7 @@
         default = mkShell {
           shellHook = ''
             # health checks for Nix flake inputs
-            nix run "github:DeterminateSystems/flake-checker"
+            nix run "github:DeterminateSystems/flake-checker/v0.2.8"
           '';
         };
       });
