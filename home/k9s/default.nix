40254c413438816df85dd4b43c387391c540656a
Franck Rasolo <franckrasolo@users.noreply.github.com>
2025-12-01 17:56:19 +0000

    make the configuration of the Nix module for k9s more granular

--- home/k9s/default.nix
+++ home/k9s/default.nix
@@ -1,5 +1,6 @@
 { config, pkgs, user, ... }:
 
+with config.lib.file;
 with pkgs.unstable;
 {
   home.packages = [
@@ -13,5 +14,17 @@ with pkgs.unstable;
     package = k9s;
   };
 
-  xdg.configFile."k9s".source = config.lib.file.mkOutOfStoreSymlink "${user.homeDirectory}/dev/dotfiles.nix/home/k9s";
+  xdg.configFile = {
+    "k9s/aliases.yaml".source = mkOutOfStoreSymlink "${user.homeDirectory}/dev/dotfiles.nix/home/k9s/aliases.yaml";
+    "k9s/config.yaml".source  = mkOutOfStoreSymlink "${user.homeDirectory}/dev/dotfiles.nix/home/k9s/config.yaml";
+    "k9s/hotkeys.yaml".source = mkOutOfStoreSymlink "${user.homeDirectory}/dev/dotfiles.nix/home/k9s/hotkeys.yaml";
+    "k9s/plugins".source      = mkOutOfStoreSymlink "${user.homeDirectory}/dev/dotfiles.nix/home/k9s/plugins";
+    "k9s/skins".source        = mkOutOfStoreSymlink "${user.homeDirectory}/dev/dotfiles.nix/home/k9s/skins";
+    "k9s/spinach.yaml".source = mkOutOfStoreSymlink "${user.homeDirectory}/dev/dotfiles.nix/home/k9s/spinach.yaml";
+    "k9s/views.yaml".source   = mkOutOfStoreSymlink "${user.homeDirectory}/dev/dotfiles.nix/home/k9s/views.yaml";
+  };
+
+  xdg.dataFile = {
+    "k9s/clusters".source = mkOutOfStoreSymlink "${user.homeDirectory}/dev/dotfiles.nix/home/k9s/clusters";
+  };
 }
