b8e032228316c1d41ef1d881e65a544609303b61
Franck Rasolo <franckrasolo@users.noreply.github.com>
2026-01-08 11:26:55 +0000

    fix the home-manager error 'conflicting managed target files: .zshenv'
    https://github.com/nix-community/home-manager/issues/7633#issuecomment-3176642404

--- home/zsh/default.nix
+++ home/zsh/default.nix
@@ -11,6 +11,9 @@
     enable = true;
     autosuggestion.enable = true;
     package = pkgs.zsh;
+    envExtra = ''
+      source ~/.zshenv.manual
+    '';
   };
 
   programs.command-not-found.enable = true;
@@ -18,8 +21,8 @@
 #  xdg.configFile."zsh".source = ./.;
   xdg.configFile."zsh".source = config.lib.file.mkOutOfStoreSymlink "${user.homeDirectory}/dev/dotfiles.nix/home/zsh";
 
-#  home.file.".zshenv".source = ./zsh/zshenv;
-  home.file.".zshenv".source = config.lib.file.mkOutOfStoreSymlink "${user.homeDirectory}/dev/dotfiles.nix/home/zsh/zshenv";
+#  home.file.".zshenv.manual".source = ./zsh/zshenv;
+  home.file.".zshenv.manual".source = config.lib.file.mkOutOfStoreSymlink "${user.homeDirectory}/dev/dotfiles.nix/home/zsh/zshenv";
 
   home.activation.zshPluginsUpdate = with lib; mkForce (hm.dag.entryAfter [ "batCache" ] ''
     export ANTIBODY_HOME="${escapeShellArg config.xdg.cacheHome}/antibody"
