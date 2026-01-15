38f2f1af77008b0e9b8cfca2364121c9a9ef0f00
Franck Rasolo <franckrasolo@users.noreply.github.com>
2026-01-15 16:37:12 +0000

    extract nix settings into a nix-darwin module

--- darwin/configuration.nix
+++ darwin/configuration.nix
@@ -4,44 +4,11 @@
   imports = [
     ./homebrew
     ./macOS
+    ./nix
     ./jankyborders
     ./skhd
     ./sops.nix
   ];
-
-  # /etc/nix/nix.conf -> /etc/static/nix/nix.conf
-
-  nix.settings = {
-    access-tokens = "";
-
-    auto-optimise-store = false;
-
-    trusted-substituters = [
-      "https://cache.iog.io" # for haskell.nix
-    ];
-
-    trusted-public-keys = [
-      "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ="
-    ];
-
-    allowed-users = [ user.accountName ];
-    trusted-users = [ user.accountName "root" ];
-
-    max-jobs = 48;  # max 3 jobs per core
-    cores    = 16;  # total number of logical cores: sysctl -n hw.ncpu
-  };
-
-  nix.extraOptions = ''
-    keep-outputs = false
-    keep-derivations = false
-    experimental-features = nix-command flakes
-  '';
-
-  nix.package = pkgs.unstable.nixVersions.latest;
-
-  nixpkgs.config.allowBroken = true;
-  nixpkgs.config.allowUnfree = true;
-
 
   # used for backwards compatibility (check the change log first)
   system.stateVersion = 4;
