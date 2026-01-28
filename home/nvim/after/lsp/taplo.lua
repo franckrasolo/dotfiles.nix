1cad7ae9a7ba1d451a7a7c09b021d647af70b796
Franck Rasolo <franckrasolo@users.noreply.github.com>
2026-01-28 13:56:04 +0100

    make the Taplo language server pick up Mise config files

---
+++ home/nvim/after/lsp/taplo.lua
@@ -0,0 +1,14 @@
+---@type vim.lsp.Config
+return {
+  root_markers = {
+    ".config/mise.toml",
+    ".config/mise/config.toml",
+    ".mise/config.toml",
+    "mise/config.toml",
+    ".mise.toml",
+    "mise.toml",
+    ".taplo.toml",
+    "taplo.toml",
+    ".git",
+  },
+}
