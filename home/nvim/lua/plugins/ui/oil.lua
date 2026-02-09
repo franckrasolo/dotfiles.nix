40bdc41a0387760cb2fe215835bad3b8d5db14a5
Franck Rasolo <franckrasolo@users.noreply.github.com>
2026-02-09 00:14:16 +0000

    make oil.nvim always open the preview by default

--- home/nvim/lua/plugins/ui/oil.lua
+++ home/nvim/lua/plugins/ui/oil.lua
@@ -18,6 +18,18 @@ local always_hidden = require("pl.List") {
   "node_modules",
   "venv",
 }
+
+-- automatically open the preview when entering an oil.nvim buffer
+vim.api.nvim_create_autocmd("User", {
+  pattern = "OilEnter",
+  callback = vim.schedule_wrap(function(args)
+    local oil = require("oil")
+    if vim.api.nvim_get_current_buf() == args.data.buf and oil.get_cursor_entry() then
+      oil.open_preview()
+    end
+  end),
+})
+
 return {
   {
     "stevearc/oil.nvim",
