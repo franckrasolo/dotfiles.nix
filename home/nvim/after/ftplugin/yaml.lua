cfaed5f6d08f5f34e7dd1fb0f407896ed1555ee1
Franck Rasolo <franckrasolo@users.noreply.github.com>
2026-02-23 12:24:03 +0000

    remove use of the `colorcolumn` option for YAML buffers

--- home/nvim/after/ftplugin/yaml.lua
+++ home/nvim/after/ftplugin/yaml.lua
@@ -1,4 +1,3 @@
-local textwidth = 120
 local indentSize = 2
 
 vim.opt_local.tabstop = indentSize
@@ -8,8 +7,7 @@ vim.opt_local.shiftwidth = indentSize
 vim.opt_local.autoindent = true
 vim.opt_local.expandtab = true
 
-vim.opt_local.textwidth = textwidth
-vim.opt_local.colorcolumn = tostring(textwidth + 1)
+vim.opt_local.textwidth = 120
 
 -- disable `smartindent` as it conflicts with Treesitter, causing unpredictable
 -- jumps or preventing `formatoptions`` from triggering correctly
