c054de1848694fd06808481f1ed5cb4f5a436dcd
Franck Rasolo <franckrasolo@users.noreply.github.com>
2026-06-01 14:17:18 +0200

    factor out package references from nixpkgs

--- darwin/security.nix
+++ darwin/security.nix
@@ -1,5 +1,6 @@
 { config, pkgs, user, ... }:
 
+with pkgs.unstable;
 {
   security = {
     pam.services.sudo_local.touchIdAuth = true;
@@ -28,7 +29,7 @@
     };
   };
 
-  system.activationScripts.addGitHubAccessToken.text = with pkgs.unstable; ''
+  system.activationScripts.addGitHubAccessToken.text = ''
     sudo ${gnused}/bin/sed -i \
       -E "s/access-tokens = .*/access-tokens = github.com=$(cat ${config.sops.templates.github-token.path})/" \
       /etc/static/nix/nix.conf
@@ -37,14 +38,14 @@
   system.primaryUser = user.accountName;
 
   # skip sudo authn for frequently used commands
-  environment.etc."sudoers.d/10-nix-commands".text = with pkgs.unstable; ''
+  environment.etc."sudoers.d/10-nix-commands".text = ''
     ${user.accountName} ALL=(ALL:ALL) NOPASSWD: \
       /run/current-system/sw/bin/darwin-rebuild, \
       /run/current-system/sw/bin/nix-build, \
       /run/current-system/sw/bin/nix-channel, \
       /run/current-system/sw/bin/nix-collect-garbage, \
-      ${pkgs.coreutils}/bin/env nix-env -p /nix/var/nix/profiles/system --set /nix/store/*, \
-      ${pkgs.coreutils}/bin/env /nix/store/*/activate, \
+      ${coreutils}/bin/env nix-env -p /nix/var/nix/profiles/system --set /nix/store/*, \
+      ${coreutils}/bin/env /nix/store/*/activate, \
       ${coreutils}/bin/cp ${_1password-cli}/bin/op /usr/local/bin/op, \
       /etc/profiles/per-user/${user.accountName}/bin/openconnect, \
       /usr/bin/dscacheutil, \
