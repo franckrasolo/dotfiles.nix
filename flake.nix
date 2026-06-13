2376b25c0b0d87a92d9dc11ca7d91d6ed4639985
Franck Rasolo <franckrasolo@users.noreply.github.com>
2026-06-13 07:15:24 +0100

    update flake URL of nix-darwin

--- flake.nix
+++ flake.nix
@@ -6,7 +6,7 @@
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
 
     nix-darwin = {
-      url = "github:LnL7/nix-darwin/nix-darwin-25.11";
+      url = "github:nix-darwin/nix-darwin/nix-darwin-25.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
