637e1ffa13ef4e8d970d22b47ff8aa9b64dee7d0
Franck Rasolo <franckrasolo@users.noreply.github.com>
2026-08-27 09:06:30 +0100

    add snippet for another frequent commit message

--- home/nvim/lua/snippets/gitcommit.lua
+++ home/nvim/lua/snippets/gitcommit.lua
@@ -4,4 +4,5 @@ return {
   s("sp", t("update Neovim spell files")),
   s("upd", t("upgrade dependencies")),
   s("upm", t("upgrade tools installed by mise")),
+  s("upmr", t("upgrade mise requirement")),
 }
