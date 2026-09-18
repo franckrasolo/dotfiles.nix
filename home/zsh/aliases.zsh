ce70e28d7c951ec9c94684f5cfa71bdb4b364178
Franck Rasolo <franckrasolo@users.noreply.github.com>
2026-09-18 23:12:18 +0100

    rebind alias for launching presenterm

--- home/zsh/aliases.zsh
+++ home/zsh/aliases.zsh
@@ -53,7 +53,7 @@ alias q="pueue"
 
 alias j="just"
 alias jc="just check"
-alias go='XDG_CONFIG_HOME=~/dev/dotfiles.nix/home presenterm $(/bin/ls slides/*.md | fzf --select-1)'
+alias przt='XDG_CONFIG_HOME=~/dev/dotfiles.nix/home presenterm $(/bin/ls slides/*.md | fzf --select-1)'
 alias tuple:renice="sudo renice -20 $(pgrep Tuple)"
 alias uuid="uuidgen | tr A-Z a-z | tr -d '\n' | pbcopy"
 
